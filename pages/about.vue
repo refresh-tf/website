b250b4c7da11ad6c85e68ad8714a0b8139c4745a
maxdup <mdupuis@hotmail.ca>
2021-12-09 02:16:55 -0500

    randomized contributor order

--- pages/about.vue
+++ pages/about.vue
@@ -1,19 +1,28 @@
 <template>
 <div class="container">
-  <div class="page_title">About Refresh!</div>
-  <div class="page_container">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</div>
+  <h1>About Refresh!</h1>
+  <div class="page_container">
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+  </div>
 
   <div class="page_title">The Team</div>
-  <div class="page_container profiles">
-    <img
-      v-for="contributor in contributors"
-      :src="'https://www.steamsignature.com/status/default/' + contributor + '.png'" alt=""/>
+  <div class="page_container contributors" ref="contributors">
+    <img v-for="contributor in contributors" alt=""
+         :src="'https://www.steamsignature.com/status/default/' + contributor + '.png'"/>
   </div>
 </div>
 </template>
 
 <script>
 import { meta } from '~/js/utils';
+
+function shuffleArr (array){
+  for (var i = array.length - 1; i > 0; i--) {
+    var rand = Math.floor(Math.random() * (i + 1));
+    [array[i], array[rand]] = [array[rand], array[i]]
+  }
+}
+
 export default {
   head() {
     let baseUrl = 'https://refresh-tf.github.io';
@@ -32,12 +41,16 @@ export default {
     const meta = await this.$content('meta').fetch()
     this.contributors = meta.contributors;
   },
+  mounted(){
+    shuffleArr(this.contributors);
+    this.$forceUpdate();
+  }
 
 }
 </script>
 
 <style lang="scss">
-.profiles {
+.contributors {
     text-align: center;
 }
 </style>
