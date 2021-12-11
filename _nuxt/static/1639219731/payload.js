3f533d508a3dbdb37b627232106e32b23616f559
maxdup <maxdup@users.noreply.github.com>
2021-12-11 10:48:52 +0000

    deploy: abc379d58eab47ff9b06ec73dfbdc77fbd059858

---
+++ _nuxt/static/1639219731/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/", (function(a,b,c,d){return {data:[{maps:[{name:a,thumbnail:"cp_process.jpeg"},{name:b,thumbnail:"cp_steel.jpg"},{name:c,thumbnail:"pl_borneo.png"},{name:d,thumbnail:"cp_metalworks.jpeg"}]}],fetch:{"0":{maps:[a,b,c,d]}},mutations:[["RESET_LAYOUT_BG",void 0]]}}("process","steel","borneo","metalworks")));
