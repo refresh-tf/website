f5ad0bc4d478c9fe762da8eb2bc3b87d76a432e5
JackyLegs <JackyLegs@users.noreply.github.com>
2022-04-27 16:39:14 +0000

    deploy: eb7a4a4288aed98fe70a7704e57f75a9650aac3d

--- _nuxt/static/1651060548/steel/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1651060548",layout:"default",error:a,state:{backgroundImg:"\u002F_nuxt\u002Fimg\u002Fcp_steel.5e214d9.png"},serverRendered:true,routePath:"\u002Fsteel",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5cd651f2"}}}}(null));
