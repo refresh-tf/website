10e3d6d5ce273c5d977c7bffef7f61b585e8d8dd
JackyLegs <JackyLegs@users.noreply.github.com>
2022-05-24 22:47:11 +0000

    deploy: 587fc2eea9db0fccb2edc682519fbd6762f94aed

---
+++ _nuxt/static/1653432404/about/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1653432404",layout:"default",error:a,state:{backgroundImg:"\u002F_nuxt\u002Fimg\u002Fbackground.4b34fac.jpg"},serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6daa6f1e"}}}}(null));
