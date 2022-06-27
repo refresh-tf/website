423367dda6dfce925844db65f504683028cc2927
JackyLegs <JackyLegs@users.noreply.github.com>
2022-06-27 07:38:21 +0000

    deploy: 5ff8c1ab362ec0739255bd2e25694c332a29d37e

--- _nuxt/static/1656315424/process/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1656315424",layout:"default",error:a,state:{backgroundImg:"\u002F_nuxt\u002Fimg\u002Fcp_process.c97bee0.jpg"},serverRendered:true,routePath:"\u002Fprocess",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"98f8dd8d"}}}}(null));
--- _nuxt/static/1656315424/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1656315424",layout:"default",error:a,state:{backgroundImg:"\u002F_nuxt\u002Fimg\u002Fbackground.4b34fac.jpg"},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"98f8dd8d"}}}}(null,"\u002F"));
