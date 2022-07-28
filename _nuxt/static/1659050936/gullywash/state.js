d96d4efe4de8cadc63c65a8814b604d3e3fc89a4
JackyLegs <JackyLegs@users.noreply.github.com>
2022-07-28 23:45:00 +0000

    deploy: 6bedac6078d61b312ae81ade034488802a9a6061

--- _nuxt/static/1659050936/gullywash/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1659050936",layout:"default",error:a,state:{backgroundImg:"\u002F_nuxt\u002Fimg\u002Fcp_gullywash.be7ebd0.jpg"},serverRendered:true,routePath:"\u002Fgullywash",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6656c30b"}}}}(null));
--- _nuxt/static/1659050936/warmtic/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1659050936",layout:"default",error:a,state:{backgroundImg:"\u002F_nuxt\u002Fimg\u002Fkoth_warmtic.2c00ca0.jpg"},serverRendered:true,routePath:"\u002Fwarmtic",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6656c30b"}}}}(null));
