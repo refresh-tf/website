227a714d1347d52135769713eb73cef6da411462
JackyLegs <JackyLegs@users.noreply.github.com>
2023-01-17 23:27:06 +0000

    deploy: 0bb8e28b557746fb24481050b84829f34dcb05dd

---
+++ _nuxt/static/1673997975/gullywash/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1673997975",layout:"default",error:a,state:{backgroundImg:"\u002F_nuxt\u002Fimg\u002Fcp_gullywash.be7ebd0.jpg"},serverRendered:true,routePath:"\u002Fgullywash",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"acc462f6"}}}}(null));
