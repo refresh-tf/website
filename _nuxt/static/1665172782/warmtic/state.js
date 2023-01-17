ecbe8a9bdefc359de82539f51461e11ca784b2c1
JackyLegs <JackyLegs@users.noreply.github.com>
2023-01-17 23:26:04 +0000

    deploy: cd53bb6eaafb966c14ec87b12411c5d575ee9d38

--- _nuxt/static/1665172782/warmtic/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1665172782",layout:"default",error:a,state:{backgroundImg:"\u002F_nuxt\u002Fimg\u002Fkoth_warmtic.2c00ca0.jpg"},serverRendered:true,routePath:"\u002Fwarmtic",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"55b0ee9e"}}}}(null));
---
+++ _nuxt/static/1673997921/warmtic/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1673997921",layout:"default",error:a,state:{backgroundImg:"\u002F_nuxt\u002Fimg\u002Fkoth_warmtic.2c00ca0.jpg"},serverRendered:true,routePath:"\u002Fwarmtic",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1b8ba303"}}}}(null));
